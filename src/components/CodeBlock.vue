a20261d0e165c31cc47cf91a81e234bbd2b48927
panghuohz <2573378837@qq.com>
2025-01-08 02:28:49 +0800

    重构CodeBlock组件，优化代码块展示，更新代码片段标题显示

--- src/components/CodeBlock.vue
+++ src/components/CodeBlock.vue
@@ -7,12 +7,14 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex bg-[#18181b] rounded-md justify-between mb-5">
-        <slot />
-        <div>
-            <button class="btn m-[8px] flex w-6 h-6 justify-center items-center">
-                <svg height="16" viewBox="0 0 16 16" version="1.1" width="16"
-                    data-view-component="true" class="octicon octicon-copy js-clipboard-copy-icon">
+    <div class="flex bg-[#18181b] rounded-md flex-col mb-5 overflow-hidden">
+        <div class="flex justify-between py-2 px-4 bg-[#202024]">
+            <div class="flex items-center">
+                <span>{{ language }}</span>
+            </div>
+            <button class="btn flex justify-center items-center">
+                <svg height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"
+                    class="octicon octicon-copy js-clipboard-copy-icon">
                     <path
                         d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z">
                     </path>
@@ -20,15 +22,16 @@ const props = defineProps({
                         d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z">
                     </path>
                 </svg>
-                <svg height="16" viewBox="0 0 16 16" version="1.1" width="16"
-                    data-view-component="true"
+                <!-- <svg height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"
                     class="octicon octicon-check js-clipboard-check-icon color-fg-success d-none">
                     <path
                         d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z">
                     </path>
-                </svg>
+                </svg> -->
             </button>
+
         </div>
+        <slot />
     </div>
 </template>
 
